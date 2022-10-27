47c16164138eb57987e5f64d49c3204ce4d8218a
Nur Intan Maghfiroh <92902574+Intannim@users.noreply.github.com>
2022-10-27 14:47:02 +0700

    Add files via upload
    Jawaban nomor 3

--- index.blade.php
+++ index.blade.php
@@ -6,6 +6,7 @@
   <body class="antialiased">
 
     <h3> UTS PWF </h3>
+
       <div class="container">
        @yield('pendaftaran')
       </div>
@@ -15,6 +16,10 @@
        <div class="container">
         @yield('nilai')
        </div>
+
+       <p> <a href="/resources/views/mahasiswa/pendaftaran.blade.php"> Ini Halaman Pendaftaran </a></p>
+       <p> <a href="/resources/views/mahasiswa/ujian.blade.php"> Ini Halaman Ujian </a></p>
+       <p> <a href="/resources/views/mahasiswa/nilai.blade.php"> Ini Halaman Nilai </a></p>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
